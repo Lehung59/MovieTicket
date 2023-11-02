c279530ca2bb585ae5bf92b392a978fffdef39b0
toanvuvv <toan.vd205173@sis.hust.edu.vn>
2023-11-02 20:38:10 +0700

    complete method movie model

--- ticketz-be/src/models/movies.model.js
+++ ticketz-be/src/models/movies.model.js
@@ -171,8 +171,97 @@ const getSingleMovies = (params) => {
   });
 };
 
+const editMovies = (data, params) => {
+  return new Promise((resolve, reject) => {
+    const dataAvail = [];
+    if (data.title != null) {
+      dataAvail.push("title=");
+    }
+    if (data.image != null) {
+      dataAvail.push("image=");
+    }
+    if (data.category != null) {
+      dataAvail.push("category=");
+    }
+    if (data.releaseDate != null) {
+      dataAvail.push("release_date=");
+    }
+    if (data.duration != null) {
+      dataAvail.push("duration=");
+    }
+    if (data.director != null) {
+      dataAvail.push("director=");
+    }
+    if (data.casts != null) {
+      dataAvail.push("casts=");
+    }
+    if (data.synopsis != null) {
+      dataAvail.push("synopsis=");
+    }
+    const dataQuery = dataAvail.map((data, i) => `${data}$${i + 1}`).join(`, `);
+    const rawValues = [
+      data.title,
+      data.image,
+      data.category,
+      data.releaseDate,
+      data.duration,
+      data.director,
+      data.casts,
+      data.synopsis,
+      params.id,
+    ];
+    const values = rawValues.filter((d) => d);
+    let sql = `update show set ${dataQuery} where id=$${values.length} RETURNING *`;
+    supabase.query(sql, values, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
 
+const deleteMovies = (params) => {
+  return new Promise((resolve, reject) => {
+    const sql = `delete from movies where id=$1`;
+    supabase.query(sql, [params.id], (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+    });
+  });
+};
 
+const updateMoviesImage = (client, req, fileLink) => {
+  return new Promise((resolve, reject) => {
+    let sqlQuery = "UPDATE movies SET ";
+    let values = [];
+    let i = 1;
+    const body = req.body;
+    if (body.password) {
+      delete body.password;
+    }
+    for (const [key, val] of Object.entries(body)) {
+      sqlQuery += `${key} = $${i}, `;
+      values.push(val);
+      i++;
+    }
+    if (req.file) {
+      sqlQuery += `image = '${fileLink}', `;
+    }
+
+    sqlQuery = sqlQuery.slice(0, -2);
+    sqlQuery += ` WHERE id = $${i} RETURNING *`;
+    values.push(req.authInfo.id);
+    console.log(sqlQuery);
+    client.query(sqlQuery, values, (error, result) => {
+      if (error) return reject(error);
+      resolve(result);
+    });
+  });
+};
 
 module.exports = {
   addMovies,
