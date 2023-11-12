eb2157430d2c53c2febdab5aec79b2080f45e8da
vu duc toan <toanv_mac@MacBook-Pro-cua-vu.local>
2023-11-13 05:23:45 +0700

    xong controller movie

--- ticketz-be/controllers/movies.controller.js
+++ ticketz-be/controllers/movies.controller.js
@@ -55,7 +55,102 @@ const getAllMovies = async (req, res) => {
     }
 };
 
+const getSingleMovies = async (req, res) => {
+    try {
+        const { params } = req;
+        const result = await moviesModel.getSingleMovies(params)
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                msg: "Movies not found"
+            });
+        };
+        res.status(200).json({
+            data: result.rows,
+            msg: "Get movies data"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Internal server error'
+        });
+    }
+}
 
+const editMovies = async (req, res) => {
+    try {
+        const { body, params } = req;
+        const result = await moviesModel.editMovies(body, params)
+        res.status(201).json({
+            data: result.rows,
+            msg: "Success update movies"
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Internal server error'
+        });
+    }
+}
+
+const deleteMovies = async (req, res) => {
+    try {
+        const { params } = req;
+        const result = await moviesModel.getSingleMovies(params)
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                msg: "Movies not found"
+            });
+        };
+        await moviesModel.deleteMovies(params)
+        res.status(201).json({
+            data: result.rows,
+            msg: "Success delete movies"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Internal server error'
+        });
+    }
+}
+
+const updateMoviesImage = async (req, res) => {
+    const client = await db.connect();
+    try {
+      await client.query("BEGIN");
+      let fileLink = "";
+      if (!req.file) {
+        return res.status(400).json({
+          msg: "Only Use Allowed Extension (JPG, PNG, JPEG, WEBP)",
+          data: null,
+        });
+      }
+      if (req.file) {
+        const fileName = req.authInfo.id;
+        const upCloud = await uploaderUsers(req, "movies", fileName);
+        fileLink = upCloud.data.secure_url;
+      }
+      const resultUserBio = await moviesModel.updateMoviesImage(
+        client,
+        req,
+        fileLink
+      );
+      await client.query("COMMIT");
+      res.status(200).json({
+        msg: "Update Success...",
+        data: resultUserBio.rows,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        msg: "Internal Server Error...",
+      });
+    } finally {
+      client.release();
+    }
+  };
 
 module.exports = {
     getAllMovies,
