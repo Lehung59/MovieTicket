5b53e6a7483dff53ff00148949b2e7be1dabf31b
chisskj <daominhchi2002@gmail.com>
2023-11-16 01:30:44 +0700

    update profile controller

--- ticketz-be/src/controllers/profile.controller.js
+++ ticketz-be/src/controllers/profile.controller.js
@@ -46,6 +46,62 @@ const updateProfile = async (req, res) => {
   }
 };
 
+const updateProfileImage = async (req, res) => {
+  const client = await db.connect();
+  try {
+    await client.query("BEGIN");
+    let fileLink = "";
+    if (!req.file) {
+      return res.status(400).json({
+        msg: "Only Use Allowed Extension (JPG, PNG, JPEG, WEBP)",
+        data: null,
+      });
+    }
+    if (req.file) {
+      const fileName = req.authInfo.id;
+      const upCloud = await uploaderUsers(req, "user", fileName);
+      fileLink = upCloud.data.secure_url;
+    }
+    const resultUserBio = await profileModel.updateProfileImage(
+      client,
+      req,
+      fileLink
+    );
+    await client.query("COMMIT");
+    res.status(200).json({
+      msg: "Update Success...",
+      data: resultUserBio.rows,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error...",
+    });
+  } finally {
+    client.release();
+  }
+};
+
+const deleteImage = async (req, res) => {
+  const client = await db.connect();
+  try {
+    const { id } = req.authInfo;
+    await client.query("BEGIN");
+    const resultUserBio = await profileModel.deleteImage(id);
+    await client.query("COMMIT");
+    res.status(200).json({
+      msg: "Update Success...",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error...",
+    });
+  } finally {
+    client.release();
+  }
+};
+
 module.exports = {
   getProfile,
   updateProfile,
