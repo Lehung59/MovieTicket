9eec6e18412335f696df63a86fa5849b6f68429b
nanh1409 <maingocanh25032002@gmail.com>
2023-11-17 20:15:17 +0700

    fix: redux update store

--- ticketz-fe/src/redux/store.js
+++ ticketz-fe/src/redux/store.js
@@ -1,18 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
+    persistReducer,
+    PAUSE,
     PERSIST,
-  } from "redux-persist";
-  
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+import reducer from "./slices";
+
+const persistConfig = {
+  key: "tickitz",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: persistedReducer,
-  devTools: true,
-  middleware: (defaultMiddleware) => {
-    return defaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [PAUSE, PERSIST],
-      },
-    });
-  },
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: (defaultMiddleware) => {
+        return defaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [PAUSE, PERSIST],
+            },
+        });
+    },
 });
 
 export default store;
