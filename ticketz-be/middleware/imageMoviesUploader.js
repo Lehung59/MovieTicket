b88a1c0fad030f441f63e1452416e8c48b4bd284
vu duc toan <toanv_mac@MacBook-Pro-cua-vu.local>
2023-11-21 08:50:14 +0700

    catch image

--- ticketz-be/middleware/imageMoviesUploader.js
+++ ticketz-be/middleware/imageMoviesUploader.js
@@ -1,19 +1,27 @@
 const { uploaderMovies } = require('../utils/cloudinary');
 
 const cloudUploadMovies = async (req, res, next) => {
-    if (req.fileValidationError) {
-        return res.status(422).json({
-            msg: req.fileValidationError
+    try {
+        
+        if (req.fileValidationError) {
+            return res.status(422).json({
+                msg: req.fileValidationError
+            })
+        }
+        const result = await uploaderMovies(req.file, "Movies", req.params.id);
+        const { data, err } = result
+        if (err) throw { msg: err };
+        if (!data) {
+            return next()
+        }
+        req.body.image = data.secure_url
+        next()
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal server error"
         })
     }
-    const result = await uploaderMovies(req.file, "Movies", req.params.id);
-    const { data, err } = result
-    if (err) throw { msg: err };
-    if (!data) {
-        return next()
-    }
-    req.body.image = data.secure_url
-    next()
 }
 
 module.exports = {cloudUploadMovies}
