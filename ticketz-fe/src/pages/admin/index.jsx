c6f4caa651c307b1288143e8ec07d96e6561dcc9
Lehung59 <nguyenlehungsc@gmail.com>
2023-12-13 02:17:20 +0700

    update admin

--- ticketz-fe/src/pages/admin/index.jsx
+++ ticketz-fe/src/pages/admin/index.jsx
@@ -11,11 +11,17 @@ import Title from "utils/wrapper/title";
 import privateRoute from "utils/wrapper/privateRoute";
 
 function Admin() {
+  const controller = useMemo(() => new AbortController(), []);
+  const router = useRouter();
+  const [datas, setDatas] = useState([]);
+
   const [image, setImage] = useState(null);
   const [location, setLocation] = useState("Select Location");
   const [addTime, setAddTime] = useState("");
   const [dataTime, setDataTime] = useState([]);
-
+  const [loading, setLoading] = useState();
+  const [seat, setSeat] = useState([]);
+  const token = useSelector((state) => state.auth.data.token);
   const showSetImage = () => {
     if (image) {
       return URL.createObjectURL(image);
@@ -39,7 +45,50 @@ function Admin() {
     updatedDataTime.splice(index, 1);
     setDataTime(updatedDataTime);
   };
-
+  const show_id = router.query.show_id;
+  // console.log(idSeat);
+  // console.log(seat);
+  // console.log(datas);
+  let seatIds = "";
+  if (datas.length !== 0 && seat.length !== 0) {
+    seatIds = datas.details
+      .filter((datas) => seat.includes(datas.seat))
+      .map((seat) => seat.seat_id);
+    // console.log(seatIds);
+  }
+  // console.log(seatIds);
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    orderSeat(seatIds, token, controller)
+      .then((res) => {
+        // console.log(res.data.data[0].transaction_id);
+        swal("Success", "Order successful", "success");
+        setTimeout(() => {
+          router.push(`/payment/${res.data.data[0].transaction_id}`);
+        }, 3000);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    setLoading(true);
+    getOrderPage(show_id, controller)
+      .then(({ data }) => {
+        setDatas(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  // console.log(datas);
   return (
     <>
       <Title title="Admin Page">
